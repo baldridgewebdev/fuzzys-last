e882041d66a8b5cc3da97afe747bea02114d060d
baldridgewebdev <153017807+baldridgewebdev@users.noreply.github.com>
2023-12-05 16:01:31 -0600

    Create fuzzys-last.php

---
+++ fuzzys-last.php
@@ -0,0 +1,21 @@
+/**
+ * Plugin Name:       Fuzzy's Last
+ * Plugin URI:        https://baldridgeweb.dev
+ * Description:       A small code sample of a silly game.
+ * Version:           0.1
+ * Author:            Jenifer
+ * GitHub Plugin URI: https://github.com/baldridgewebdev/fuzzys-last
+ */
+
+class Fuzzys_Last{
+  public function __construct(  ) {
+		$this->load_game();
+    $this->init_ajax();
+	}
+  private function load_game(){
+    
+  }
+  private init_ajax(){
+    
+  }
+}
