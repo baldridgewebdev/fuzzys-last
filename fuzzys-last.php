3ed5a17e4d4224e92215fb75eea9c495e8e3949d
baldridgewebdev <153017807+baldridgewebdev@users.noreply.github.com>
2023-12-08 15:12:32 -0600

    Update fuzzys-last.php
    added seed functionality.

--- fuzzys-last.php
+++ fuzzys-last.php
@@ -10,13 +10,40 @@
  */
 
 class Fuzzys_Last{
+  private $cookie_name = 'fuzzys_last';
   public function __construct(  ) {
-      $this->load_game();
       $this->init_ajax();
     }
   private function load_game(){
-    $this->set_seed();
+    
   }
+  private function load_seed_cookie(){
+    $seed = $_COOKIE[$this->cookie_name];
+    if(isset($seed)){
+      $this->seed = $this->convert_seed_to_indexs($seed);
+      return true;
+    }
+    return false;
+  }
+  private function set_seed_cookie($seed){
+    setcookie($this->cookie_name, $seed, time() + (86400 * 30), "/");
+  }
+  private function generate_new_seed($seed_length=5){
+    $seed = "";
+    for($i = 0; $i < $seed_length; $i++;){
+      $seed += $this->generate_seed_part();
+    }
+    return $seed;
+  }
+  private function generate_seed_part(){
+    return str_pad(rand(0, 99), 2, '0', STR_PAD_LEFT);
+  }
+  private function convert_seed_to_indexs($seed){
+    $indexes = str_split($seed,2);
+    $indexes = array_map('intval',$indexes);
+    return $indexes;
+  }
+
   private function fuzzy_search($query, $items, $threshold = 2) {
     $results = [];
 
@@ -37,6 +64,7 @@ class Fuzzys_Last{
     add_action( 'wp_ajax_nopriv_guess', array($this,'ajax_guess') );
   }
   private function ajax_guess(){
+    $this->load_game();
     die();
   }
 }
