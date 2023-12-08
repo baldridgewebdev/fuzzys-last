cfa2ad2cd3d70cec614ca66db18b1ea730042db9
baldridgewebdev <153017807+baldridgewebdev@users.noreply.github.com>
2023-12-07 20:59:32 -0600

    Update fuzzys-last.php
    Added fuzzy search

--- fuzzys-last.php
+++ fuzzys-last.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Plugin Name:       Fuzzy's Last
  * Plugin URI:        https://baldridgeweb.dev
@@ -10,21 +11,33 @@
 
 class Fuzzys_Last{
   public function __construct(  ) {
-		$this->load_game();
-    $this->init_ajax();
-	}
+      $this->load_game();
+      $this->init_ajax();
+    }
   private function load_game(){
-    $this->fuzzy = $this->load_fuzzy();
+    $this->set_seed();
   }
-  private function load_fuzzy(){
-    require_once __DIR__ . "/fuzzy.php";
+  private function fuzzy_search($query, $items, $threshold = 2) {
+    $results = [];
+
+    foreach ($items as $item) {
+      $distance = levenshtein(strtolower($query), strtolower($item));
+
+      if ($distance <= $threshold) {
+        $results[$item] = $distance;
+      }
+    }
+
+    asort($results); // Sort results by distance (lower is better)
+
+    return $results;
   }
-  private init_ajax(){
+  private function init_ajax(){
     add_action( 'wp_ajax_guess', array($this,'ajax_guess') );
-		add_action( 'wp_ajax_nopriv_guess', array($this,'ajax_guess') );
+    add_action( 'wp_ajax_nopriv_guess', array($this,'ajax_guess') );
   }
-	private ajax_guess(){
-		die();
-	}
+  private function ajax_guess(){
+    die();
+  }
 }
 $fuzzys = new Fuzzys_Last();
