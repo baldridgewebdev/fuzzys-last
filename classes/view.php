b06720731d160f5267b9c70debce2459a254d346
baldridgewebdev <153017807+baldridgewebdev@users.noreply.github.com>
2023-12-10 06:08:32 -0600

    cleaned up the gameboard

--- classes/view.php
+++ classes/view.php
@@ -4,23 +4,24 @@ class Fuzzys_Last_View{
   function __construct(){
     
   }
-  public function fuzzys_last_gameboard($word_count){
+  public function fuzzys_last_gameboard(){
     $html = '
     <div class="fuzzys-last">
       <div class="gameboard">
-        <div class="words">';
-		for($i = 1; $i <= $word_count; $i++){
-			$html .= '<div id="fuzzy-word-'.$i.'" class="word"></div>';
-		}
-    $html = '</div>
-		  <form class="fuzzy-form">
-			<div class="fuzzy-inputs"';
-		for($i = 1; $i <= $word_count; $i++){
-			$html .= '<input id="fuzzy-guess-char-'.$i.'" type="text" name="fuzzy-input'.$i.'" maxlength="1" >';
-		}
-    $html = '</div><div class="button-area">
+        <div class="words">
+          <div id="fuzzy-word-1" class="word"></div>
+          <div id="fuzzy-word-2" class="word"></div>
+          <div id="fuzzy-word-3" class="word"></div>
+          <div id="fuzzy-word-4" class="word"></div>
+          <div id="fuzzy-word-5" class="word"></div>
+        </div>
+        <form class="fuzzy-input">
+          <input id="fuzzy-guess-char-1" type="text" name="input1" maxlength="1" >
+          <input id="fuzzy-guess-char-2" type="text" name="input2" maxlength="1" >
+          <input id="fuzzy-guess-char-3" type="text" name="input3" maxlength="1" >
+          <input id="fuzzy-guess-char-4" type="text" name="input4" maxlength="1" >
+          <input id="fuzzy-guess-char-5" type="text" name="input5" maxlength="1" >
           <button name="submit" class="submit">Submit</button> <button name="new-game" class="new-game">New Game</button>
-		  </div>
         </form>
       </div>
       <div class="instructions">
