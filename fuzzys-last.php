6368079499a6c24129185b5483bf932d97ff821e
baldridgewebdev <153017807+baldridgewebdev@users.noreply.github.com>
2023-12-11 08:40:59 -0600

    Implemented backend game logic

--- fuzzys-last.php
+++ fuzzys-last.php
@@ -26,6 +26,7 @@ class Fuzzys_Last{
   private function init_scripts(){
     add_action( 'wp_enqueue_scripts', function(){
       wp_enqueue_script( 'fuzzys-last-behaviours', plugins_url( '/assets/behaviours.js' , __FILE__ ), array( 'jquery' ) );
+	  wp_localize_script('fuzzys-last-behaviours', 'alfred', array('ajax_url' => admin_url('admin-ajax.php')));
       wp_enqueue_style( 'fuzzys-last-style', plugins_url( '/assets/style.css' , __FILE__ ));
     } );
   }
@@ -33,32 +34,33 @@ class Fuzzys_Last{
     add_shortcode('fuzzys_last', array($this,'fuzzys_last_shortcode'));
   }
   public function fuzzys_last_shortcode(){
-    $this->view->fuzzys_last_gameboard();
+    return $this->view->fuzzys_last_gameboard();
   }
   private function init_ajax(){
     add_action( 'wp_ajax_guess', array($this,'ajax_guess') );
     add_action( 'wp_ajax_nopriv_guess', array($this,'ajax_guess') );
   }
   private function load_game(){
-    
+    $this->model = new Fuzzys_Last_Model($this->cookie_name);
   }
-  private function fuzzy_search($query, $items, $threshold = 2) {
+  private function fuzzy_search($query, $items) {
     $results = [];
 
     foreach ($items as $item) {
       $distance = levenshtein(strtolower($query), strtolower($item));
 
-      if ($distance <= $threshold) {
-        $results[$item] = $distance;
-      }
+        $results[] = $distance;
     }
-
-    asort($results); // Sort results by distance (lower is better)
+	if(empty($results)) return $results;
 
     return $results;
   }
-  private function ajax_guess(){
+  public function ajax_guess(){
     $this->load_game();
+	$query = substr($_POST['guess'],0,5);
+	$items = $this->model->get_game_word_list();
+	$results = $this->fuzzy_search($query, $items);
+	echo json_encode($results);
     die();
   }
 }
